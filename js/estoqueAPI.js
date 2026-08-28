0af1a6e366845fbc8a2b73d62704e3c9abec016f
User <carvalho2006elias@gmail.com>
2026-08-27 22:02:51 -0300

    update

--- js/estoqueAPI.js
+++ js/estoqueAPI.js
@@ -3,15 +3,15 @@ function addSkuRow(sku) {
     tr.innerHTML = `
         <td class="produto">${sku.nome}</td>
         <td class="categoria">${sku.categoria}</td>
-        <td class="qnt_estoque">${sku.qnt_estoque}</td>
+        <td class="qnt_estoque">${sku.qnt}</td>
         <td class="valor">${sku.valor}</td>
         <td class="valor_venda">${sku.valor_venda}</td>
-        <td class="total_estoque">${sku.total_estoque}</td>
-        <td class="status">${sku.status}</td>
+        <td class="total_estoque">${check_valor_em_estoque(sku.qnt, sku.valor)}</td>
+        <td class="status">${check_status(sku.qnt_min, sku.qnt)}</td>
         <td>
             <div class="row-actions"> 
                 <button id='editar-sku-btn' onclick='edit_sku_open(${JSON.stringify(sku)})'><svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M12 20h9"></path><path d="M16.5 3.5a2.1 2.1 0 0 1 3 3L7 19l-4 1 1-4Z"></path></svg></button>
-                <button><svg width="14" height="14" onclick='' viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"></path><path d="M10 11v6"></path><path d="M14 11v6"></path><path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"></path></svg></button>
+                <button><svg width="14" height="14" onclick='deletar_sku(${sku.id})' viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6l-1 14a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2L5 6"></path><path d="M10 11v6"></path><path d="M14 11v6"></path><path d="M9 6V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2"></path></svg></button>
             </div>
         </td>
     `;
@@ -19,6 +19,26 @@ function addSkuRow(sku) {
     body.appendChild(tr);
 }
 
+
+function check_valor_em_estoque(qnt, valor) {
+  return  '$' + (qnt * valor)
+}
+function check_status(qnt_min, qnt) {
+  qnt_min += 0
+  qnt += 0
+  if (qnt < 0) {
+    return 'NEGATIVO'
+  } 
+  else if (qnt == 0) {
+    return 'Fora de estoque'
+  }
+  else if (qnt < qnt_min) {
+    return 'Baixo estoque'
+  }
+  else if (qnt > qnt_min) {
+    return 'Em estoque!'
+  }
+}
 
 function limpar_tabela_estoque() {
     var body = document.getElementById('estoque-table-body');
@@ -33,24 +53,34 @@ function edit_sku_open(sku) {
   resgatar_dados_sku(sku)
   sku_editando_id = sku['id']
 }
+
 function resgatar_dados_sku(sku) {
   document.getElementById('editar-sku-nome').value = sku['nome']
   document.getElementById('editar-sku-categoria').value = sku['categoria']
-//   document.getElementById('editar-item-unidade').value = sku['unidade']
-  document.getElementById('editar-item-qnt').value = sku['qnt_estoque']
+  document.getElementById('editar-item-qnt').value = sku['qnt']
   document.getElementById('editar-item-preco-un').value = sku['valor']
   document.getElementById('editar-item-preco-venda').value = sku['valor_venda']
 }
 
 async function editar_sku() {
-    var data = extract_form('editar-sku-nome', 'editar-sku-categoria', 'editar-item-qnt', 'editar-item-preco-un', 'editar-item-preco-venda')
+    console.log('clicado editar')
+    var data = extract_form('editar-sku-nome', 'editar-sku-categoria', 'editar-item-qnt', 'editar-item-preco-un', 'editar-item-preco-venda', 'item-qnt-min')
     console.log('data extraida, data: ${}')
     if (!sku_editando_id) {
         return
     }
-    swap_modal('modal-clientes');
-    await edit_estoque_db(sku_editando_id, data)
-    await listarEstoque();
+    var dataValidacao = {
+        'sku-nome': data['editar-sku-nome'],
+        'sku-categoria': data['editar-sku-categoria'],
+        'item-qnt': data['editar-item-qnt'],
+        'item-preco-un': data['editar-item-preco-un']
+    }
+    console.log('data validacao extraida')
+    if (validar_sku(dataValidacao)) {
+        swap_modal('modal-clientes');
+        await edit_estoque_db(sku_editando_id, data)
+        await listarEstoque();
+    }
 }
 
 async function edit_estoque_db(id, sku) {
@@ -67,6 +97,7 @@ async function edit_estoque_db(id, sku) {
         qnt: sku['editar-item-qnt'],
         valor: sku['editar-item-preco-un'],
         valor_venda: sku['editar-item-preco-venda'],
+        qnt_min: sku['item-qnt-min'],
         status: 'PLACEHOLDER',
       })
     });
@@ -79,16 +110,45 @@ async function edit_estoque_db(id, sku) {
 }
 
 
+function validar_sku(data) {
+  var validar_data = {
+    'nome':data['sku-nome'],
+    'categoria':data['sku-categoria'],
+    'qnt':data['item-qnt'],
+    'valor':data['item-preco-un']
+  }
+  console.log(validar_data)
 
+  if (validar_data.nome == '') {
+    console.log(data)
+    alert('Preencha o nome do novo produto')
+    return false
+  }
+  if (validar_data.categoria == '') {
+    alert('Preencha a categoria do novo produto')
+    return false
+  }
+  if (validar_data.qnt == '') {
+    alert('Preencha a quantidade do novo produto')
+    return false
+  }
+  if (validar_data.valor == ''){
+    alert('Preencha o valor do novo produto')
+    return false
+  }
+  return true
+}
 
 
 async function add_sku() {
-    var data = extract_form('sku-nome', 'sku-categoria', 'item-qnt', 'item-preco-un', 'item-preco-venda')
-
+    var data = extract_form('sku-nome', 'sku-categoria', 'item-qnt', 'item-preco-un', 'item-preco-venda', 'item-qnt-min')
+    if (!validar_sku(data)){
+      return
+    }
     swap_modal('modal-clientes');
     document.getElementById('register-sku-form').reset()
     await cadastrarSku(data)
-    await loadEstoque();
+    await listarEstoque();
     return true
 }
 
@@ -108,6 +168,7 @@ async function cadastrarSku(sku) {
         valor: sku['item-preco-un'],
         valor_venda: sku['item-preco-venda'],
         status: 'PLACEHOLDER',
+        qnt_min: sku['item-qnt-min']
       })
     });
 
@@ -134,10 +195,31 @@ async function loadEstoque(filtro = "") {
 }
 
 async function listarEstoque(filtro = ''){
-    var estoque = await loadEstoque(filtro);
-    limpar_tabela_estoque()
-    for(let sku of estoque){
-        addSkuRow(sku)
-    }
+  var estoque = await loadEstoque(filtro);
+  limpar_tabela_estoque()
+  for(let sku of estoque){
+      addSkuRow(sku)
+  }
 }
+
+async function deletar_sku_db(id) {
+  try {
+    const resposta = await fetch(`https://fcsistemas-gestao.onrender.com/estoque/${id}`, {
+      method: "DELETE"
+    });
+    const dados = await resposta.json();
+    return dados;
+  } catch (erro) {
+    console.error("Erro ao deletar sku:", erro);
+  }
+}
+
+async function deletar_sku(id) {
+  if (!confirm('Tem certeza que deseja excluir este item?')) {
+    return
+  }
+  await deletar_sku_db(id)
+  await listarEstoque();
+}
+
 listarEstoque();
